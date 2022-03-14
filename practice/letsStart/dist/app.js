07ecd8f066e29668074123616172f968f6e17b24
nwnp <jjw_0418@naver.com>
2022-03-14 18:45:56 +0900

    feat: middleware
     - app.use
     - route
     - Not Found error

--- practice/letsStart/dist/app.js
+++ practice/letsStart/dist/app.js
@@ -3,10 +3,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app_model_1 = require("./app.model");
 var app = express();
-var data = [1, 2, 3];
+app.use(function (req, res, next) {
+    console.log("this is logging middleware: " + req.rawHeaders[1]);
+    next();
+});
 app.get("/", function (req, res) {
-    console.log(req);
     res.send({ cats: app_model_1.Cat });
+});
+app.get("/cats/blue", function (req, res) {
+    res.send({ cats: app_model_1.Cat, blue: app_model_1.Cat[0] });
+});
+app.get("/cats/som", function (req, res) {
+    res.send({ som: app_model_1.Cat[0] });
+});
+app.use(function (req, res, next) {
+    var error = {
+        status: "404",
+        msg: "Not Found Page",
+    };
+    console.log("this is error middleware");
+    res.send("<h1>" + error.status + " " + error.msg + "</h1>");
 });
 app.listen(8080, function () {
     console.log("Server is on...");
--- practice/letsStart/src/app.ts
+++ practice/letsStart/src/app.ts
@@ -3,11 +3,32 @@ import { Cat, CatType } from "./app.model";
 
 const app: express.Express = express();
 
-const data = [1, 2, 3];
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log(`this is logging middleware: ${req.rawHeaders[1]}`);
+    next();
+  }
+);
 
 app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
   res.send({ cats: Cat });
+});
+
+app.get("/cats/blue", (req, res) => {
+  res.send({ cats: Cat, blue: Cat[0] });
+});
+
+app.get("/cats/som", (req, res) => {
+  res.send({ som: Cat[0] });
+});
+
+app.use((req, res, next) => {
+  const error = {
+    status: "404",
+    msg: "Not Found Page",
+  };
+  console.log("this is error middleware");
+  res.send(`<h1>${error.status} ${error.msg}</h1>`);
 });
 
 app.listen(8080, () => {

a9e97effc62920d30333b220783f04f3d7b69776
nwnp <jjw_0418@naver.com>
2022-06-09 13:36:10 +0900

    feat: mongodb connection

--- first-project/src/app.module.ts
+++ first-project/src/app.module.ts
@@ -3,9 +3,15 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

8e1778c5054980ef3ea24980a9ba38f6b81aafef
nwnp <jjw_0418@naver.com>
2022-06-17 23:35:16 +0900

    feat: Create comment schema

---
+++ first-project/src/comments/comments.schema.ts
@@ -0,0 +1,58 @@
+import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsPositive, IsString } from 'class-validator';
+import { Document, Types } from 'mongoose';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class Comments extends Document {
+  @ApiProperty({
+    description: 'email',
+    required: true,
+  })
+  @Prop({
+    type: Types.ObjectId,
+    required: true,
+    ref: 'cats',
+  })
+  @IsNotEmpty()
+  author: string;
+
+  @ApiProperty({
+    description: '댓글 컨텐츠',
+    required: true,
+  })
+  @Prop({
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  contents: string;
+
+  @ApiProperty({
+    description: '좋아요 수',
+  })
+  @Prop({
+    default: 0,
+    required: true,
+  })
+  @IsPositive()
+  likeCount: string;
+
+  @ApiProperty({
+    description: '작성 대상(게시글, 정보글)',
+    required: true,
+  })
+  @Prop({
+    type: Types.ObjectId,
+    required: true,
+    ref: 'cats',
+  })
+  @IsNotEmpty()
+  info: Types.ObjectId;
+}
+
+export const CommentsSchema = SchemaFactory.createForClass(Comments);
---
+++ first-project/src/comments/services/comments.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CommentsService {
+  async getAllcomments() {}
+  async createComment(id: string) {}
+}

b37dc500580c46c42f91042ec613a045cfe13043
nwnp <jjw_0418@naver.com>
2022-06-18 00:25:27 +0900

    feat: add comment & like

--- first-project/src/comments/comments.module.ts
+++ first-project/src/comments/comments.module.ts
@@ -1,8 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { Module, forwardRef } from '@nestjs/common';
 import { CommentsController } from './controllers/comments.controller';
 import { CommentsService } from './services/comments.service';
+import { Comments, CommentsSchema } from './comments.schema';
+import { CatsModule } from 'src/cats/cats.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Comments.name, schema: CommentsSchema },
+    ]),
+    forwardRef(() => CatsModule),
+  ],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
--- first-project/src/comments/services/comments.service.ts
+++ first-project/src/comments/services/comments.service.ts
@@ -1,7 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { CatsRepository } from 'src/cats/cats.repository';
+import { CommentCreateDto } from './../dto/comments.create.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Comments } from '../comments.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class CommentsService {
-  async getAllcomments() {}
-  async createComment(id: string) {}
+  constructor(
+    @InjectModel(Comments.name) private readonly commentsModel: Model<Comments>,
+    private readonly catsRepository: CatsRepository,
+  ) {}
+
+  async getAllcomments() {
+    try {
+      const comments = await this.commentsModel.find();
+      return comments;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+  async createComment(id: string, commentData: CommentCreateDto) {
+    try {
+      const targetCat = await this.catsRepository.findCatByIdWithoutPassword(
+        id,
+      );
+      const { contents, author } = commentData;
+      const validatedAuthor =
+        await this.catsRepository.findCatByIdWithoutPassword(author);
+      const newComment = new this.commentsModel({
+        author: validatedAuthor._id,
+        contents,
+        info: targetCat._id,
+      });
+      return await newComment.save();
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async plusLike(id: string) {
+    try {
+      const comment = await this.commentsModel.findById(id);
+      comment.likeCount += 1;
+      return await comment.save();
+    } catch (error) {}
+  }
 }
